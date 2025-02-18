cd7a6615b8da5400caad1e8e2ad43bc5b1578dd4
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-02-18 09:37:37 +0700

    Complete basic requirements of saving state between

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,20 @@ import { useState } from "react";
 function App() {
   const [submitted, setSubmitted] = useState(false);
   const [userData, setUserData] = useState({});
+
   let content;
 
   function handleSubmit(e) {
     e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
-    const formJson = Object.fromEntries(formData.entries());
-    setUserData(formJson);
-    setSubmitted(!submitted);
+    if (!submitted) {
+      const form = e.target;
+      const formData = new FormData(form);
+      const formJson = Object.fromEntries(formData.entries());
+      setUserData(formJson);
+      setSubmitted(!submitted);
+    } else {
+      setSubmitted(!submitted);
+    }
   }
 
   if (!submitted) {
@@ -37,8 +42,8 @@ function App() {
       <>
         <h1>Here is your CV!</h1>
         <hr />
-        <Display userData={userData}></Display>
         <form method="post" onSubmit={handleSubmit}>
+          <Display userData={userData}></Display>
           <input type="submit" value="Edit" />
         </form>
       </>
